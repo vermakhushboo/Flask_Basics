42bee01cb8ca7406e6e9b89bb17c18dff6b5f2e5
vermakhushboo <43381712+vermakhushboo@users.noreply.github.com>
2022-03-25 20:31:46 +0530

    Route patterns and template inheritance

--- application/routes.py
+++ application/routes.py
@@ -3,7 +3,19 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template("index.html", login=False)
+    return render_template("index.html", login=True)
+
+@app.route('/courses')
+def courses():
+    return render_template("courses.html", login=True)
+
+@app.route('/register')
+def register():
+    return render_template("register.html", login=True)
+
+@app.route('/login')
+def login():
+    return render_template("login.html", login=True)
 
 @app.route('/hello')
 def hello():
